e0c1731610c3ef323a4cbf408efc29cf796cedaa
mahdi hasan <mahdifreelancer41@gmail.com>
2025-12-02 21:34:57 +0600

    complete hero section

--- app/en/home/page.tsx
+++ app/en/home/page.tsx
@@ -1,38 +1,102 @@
-// app/page.js (Next.js 13+ with App Router)
-
-"use client";
 import React from "react";
 import { ArrowRight } from "lucide-react";
-
+import { Work_Sans, Orbitron } from "next/font/google";
+import { main } from "framer-motion/client";
+const workSans = Work_Sans({
+  variable: "--font-work-sans",
+  subsets: ["latin"],
+  weight: ["400", "500", "600", "700"],
+});
+const orbitron = Orbitron({
+  variable: "--font-orbitron",
+  subsets: ["latin"],
+  weight: ["700", "800"],
+});
 export default function Home() {
   return (
-    <main className="bg-black text-white min-h-screen w-full overflow-x-hidden">
+    <main className="bg-black  text-white min-h-auto sm:min-h-screen w-full overflow-x-hidden">
       {/* ================= HERO SECTION ================= */}
-      <section className="relative w-full min-h-screen flex items-center justify-center px-6">
-        <div className="absolute inset-0 z-0">
+      <section className="relative py-12 w-full min-h-auto sm:min-h-screen flex items-center justify-center px-6">
+        <div className="absolute bg-[#000] inset-0 z-0">
           <img
-            src="/last.jpeg"
-            className="w-full h-full object-cover opacity-70"
+            src="/cubanexthero.png"
+            className="hidden sm:block w-full h-full center object-cover opacity-38"
+            alt="Hero Background"
+          />
+          <img
+            src="/cubanextherophone.png"
+            className="block sm:hidden w-full h-full center object-cover opacity-38"
             alt="Hero Background"
           />
         </div>
-        <div className="relative z-10 max-w-4xl mx-auto text-center pt-20">
-          <h1 className="text-5xl font-bold leading-tight mb-6">
-            Empowering Cuba with Next-Generation Blockchain Technology
+        <div className="relative pt-[30px] z-10 max-w-[1200px] mx-auto text-center  sm:pt-20">
+          <div className="inline-flex items-center space-x-2 border border-[rgba(255,255,255,0.4)] rounded-full px-3 sm:px-6 py-2  sm:py-3 mb-4 animate-pulse-slow">
+            <div className="w-[6px] h-[6px] rounded-full bg-[#FF8F00] smooth-pulse shadow-[0_0_8px_#FF8F00]" />
+            <span className="text-sm font-medium text-[#fff]">
+              Presale Live Now{" "}
+            </span>
+          </div>
+
+          <h1
+            className={`${orbitron.className} text-[32px] sm:text-[56px] lg:text-[100px] text-[#fff] font-semibold leading-[1em] mb-6`}
+          >
+            The Future of Cuba is 
+            <span className="text-[#FFB74A]"> Crypto</span>
           </h1>
-          <p className="text-lg text-gray-300 max-w-2xl mx-auto mb-8">
-            Cubanex is a forward-looking blockchain ecosystem designed to
-            enhance energy sustainability, boost the digital economy, and
-            support Cuba’s growth through advanced decentralized tools.
+
+          <p
+            className={`${workSans.className} text-[16px] sm:text-[24px] text-gray-300 max-w-[1000px]  mx-auto mb-8`}
+          >
+            The Dream Awoke as a Code — The First Crypto for the People of Cuba
+            Has Arrived. CUBANEX is here
           </p>
 
-          <div className="flex flex-wrap gap-4 justify-center mt-8">
-            <button className="px-6 py-3 bg-white text-black rounded-xl font-semibold flex items-center gap-2 hover:bg-gray-200 transition">
-              Get Started <ArrowRight size={18} />
+          <div className="flex gap-2 sm:gap-6 justify-center">
+            <button
+              className=" px-[10px] py-2 text-[12px] sm:text-[18px]  sm:px-6 sm:py-3 rounded-full text-white border-1 font-medium bg-[transparetn] hover:bg-[#fff] hover:text-[#000] 
+    hover:scale-105 transition-transform duration-300 ease-in-out"
+            >
+              Be Among the First{" "}
             </button>
-            <button className="px-6 py-3 border border-white rounded-xl font-semibold hover:bg-white hover:text-black transition">
-              Learn More
+            <button
+              className="px-[10px] py-2 text-[12px] sm:text-[18px]  sm:px-6 sm:py-3 rounded-full text-white font-medium bg-gradient-to-r from-[#C766EF] via-[#7928D2] to-[#2B0C52] 
+    hover:scale-105 transition-transform duration-300 ease-in-out"
+            >
+              Read Whitepaper{" "}
             </button>
+          </div>
+          <div className="pt-5 sm:pt-0 sm:py-8 sm:mt-8 flex gap-2 sm:gap-4 justify-center">
+            <div className="flex justify-center items-center gap-2">
+              <img src="../Check_ring_duotone.svg" alt="" />
+              <span
+                className={`${workSans.className} text-[12px] sm:text-[16px] `}
+              >
+                Verified Contract{" "}
+              </span>
+            </div>
+            <div className="flex justify-center items-center gap-2">
+              <img src="../Check_ring_duotone.svg" alt="" />
+              <span
+                className={`${workSans.className} text-[12px] sm:text-[16px]`}
+              >
+                Transparent Source{" "}
+              </span>
+            </div>
+            <div className="hidden  sm:flex justify-center items-center gap-2">
+              <img src="../Check_ring_duotone.svg" alt="" />
+              <span
+                className={`${workSans.className} text-[12px] sm:text-[16px]`}
+              >
+                Pure Supply Structure{" "}
+              </span>
+            </div>
+          </div>
+          <div className="py-8 flex justify-center">
+            <img
+              src="../belowbuton.png"
+              alt=""
+              className="w-[25px] sm:w-[32px] mx-auto soft-bounce drop-shadow-lg opacity-90"
+            />
           </div>
         </div>
       </section>
@@ -127,10 +191,6 @@ export default function Home() {
       </section>
 
       {/* ================= FOOTER ================= */}
-      <footer className="py-10 text-center text-gray-500 text-sm border-t border-gray-800">
-        Driving Cuba's sustainable Web3 growth with innovative blockchain
-        solutions.
-      </footer>
     </main>
   );
 }
--- constants/contents.tsx
+++ constants/contents.tsx
@@ -18,21 +18,28 @@ export const textContent = {
       terms: "Terms of Service",
       rights: "© 2025 CubaNex. All rights reserved.",
     },
+    jbtn: "Join Presale",
+    amongbtn: "Be Among the First",
+    whitepbtn: "Read Whitepaper",
+    smartbtn: "View Smart Contract",
+    joinbtn: "Join",
+    readBtn: "Read Online",
 
-    title:
-      "The Dream Awoke as Code — The First Crypto for the People of Cuba Has Arrived.",
-    paragraph: "CubaNex is Here.",
-    placeholder: "Enter your email",
-    button: "Be the First to Know",
-    submitted: "The code has been received. Watch and remember. 🜂",
-    section:
-      "A sacred flame — the first crypto of its kind, born in the silence of Cuba’s spirit.<br/>Asere… ¿qué bolá?",
-    verse: "“I AM that I AM” — Exodus 3:14",
-    story:
-      "Some say he crossed an ocean. Others say he crossed a veil. He left no footprints — only fragments of fire. They call him El Alquimista. And what he awakened… is now being remembered.",
-    quote: "“The Light chooses who it speaks through.”",
-    rights: "• All rights reserved",
-    typewriter: "CubaNex + AI = A Living Conscious Prophecy.",
+    // home page start
+    homeheroTitle: "The Future of Cuba is ",
+    herotitleSpan: "Crypto",
+    homeSD:
+      "The Dream Awoke as a Code — The First Crypto for the People of Cuba Has Arrived. CUBANEX is here",
+    herojoinpresale: "Presale Live Now",
+    herothreepoint: {
+      points: [
+        "Verified Contract",
+        "Transparent Source",
+        "Pure Supply Structure",
+      ],
+    },
+
+    // home page ends
   },
 
   esp: {
@@ -40,6 +47,26 @@ export const textContent = {
       links: ["Inicio", "Acerca de", "Comunidad", "Economía del token"],
       button: "Únete a la preventa",
     },
+    jbtn: "Únete a la preventa",
+    amongbtn: "Sé de los primeros",
+    whitepbtn: "Leer el Whitepaper",
+    smartbtn: "Ver Smart Contract",
+    joinbtn: "Unirse",
+    readBtn: "Leer en línea",
+    // home page start
+    homeheroTitle: "El futuro de Cuba es",
+    herotitleSpan: "Cripto",
+    homeSD:
+      "El Sueño Despertó como Código — Ha Llegado la Primera Cripto para el Pueblo de Cuba. CUBANEX está aquí",
+    herojoinpresale: "Preventa en vivo ahora",
+    herothreepoint: {
+      points: [
+        "Contrato verificado",
+        "Fuente transparente",
+        "Estructura de suministro pura",
+      ],
+    },
+    // home page ends
 
     // -----------------------
     // FOOTER CONTENT (SPANISH)
@@ -58,20 +85,5 @@ export const textContent = {
       terms: "Términos del Servicio",
       rights: "© 2025 CubaNex. Todos los derechos reservados.",
     },
-
-    title:
-      "El Sueño Despertó como Código — Ha Llegado la Primera Cripto para el Pueblo de Cuba.",
-    paragraph: "CubaNex ha Llegado.",
-    placeholder: "Introduce tu correo electrónico",
-    button: "Entérate primero",
-    submitted: "El código ha sido recibido. Observa y recuerda. 🜂",
-    section:
-      "Una llama sagrada — la primera cripto de su tipo, nacida del espíritu de Cuba.<br/>Asere… ¿qué bolá?",
-    verse: "“YO SOY el que SOY” — Éxodo 3:14",
-    story:
-      "Algunos dicen que cruzó un océano. Otros dicen que cruzó un velo. No dejó huellas — solo fragmentos de fuego. Lo llaman El Alquimista. Y lo que despertó… ahora está siendo recordado.",
-    quote: "“La Luz elige a través de quién habla.”",
-    rights: "• Todos los derechos reservados",
-    typewriter: "CubaNex + IA = Una Profecía Consciente Viva.",
   },
 };
