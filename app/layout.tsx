659946ae8d115d9beb36e4686c8426949e6bdb6d
mahdi hasan <mahdifreelancer41@gmail.com>
2026-03-01 23:59:51 +0600

    added cubanex-token-v2-32x32.svg and fix env

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,7 +6,6 @@ import "./globals.css";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import BackToTop from "@/components/BacktoTop";
-import ChatBot from "@/components/ChatBot";
 
 // Fonts
 const geistSans = Geist({ variable: "--font-geist-sans", subsets: ["latin"] });
@@ -46,7 +45,6 @@ export default function RootLayout({
       >
         <Header defaultLanguage={defaultLang} />
         <BackToTop />
-        <ChatBot />
         {children}
 
         <Footer />
